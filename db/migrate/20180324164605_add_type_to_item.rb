00ada561d3860cbaabdf5cbdf1a267aaaba7eaa7
Reid Laitinen <reidlaitinen@gmail.com>
2018-03-24 13:20:30 -0600

    fix db stuff

--- db/migrate/20180324164605_add_type_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToItem < ActiveRecord::Migration[5.1]
-  def change
-    add_column :items, :type, :string
-  end
-end
--- db/migrate/20180324183322_rename_type_to_category.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeToCategory < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :items, :type, :category
-  end
-end
