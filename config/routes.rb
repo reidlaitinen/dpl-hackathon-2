8ebc58e49f78f40ce04e4dfaabb38039a50d5a4c
Reid Laitinen <reidlaitinen@gmail.com>
2018-03-24 10:15:18 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static/index'
+
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  namespace :api do
+    #API ROUTES SHOULD GO HERE
+  end
+
+  #Do not place any routes below this one
+  get '*other', to: 'static#index'
+end
