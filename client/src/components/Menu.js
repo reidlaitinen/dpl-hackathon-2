ce8801025a49569906915964dd608d72ce65f13f
Alex Shapiro <acjshapiro@gmail.com>
2018-03-24 13:50:05 -0600

    menu sushi components

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,7 +1,33 @@
 import React from 'react';
+<<<<<<< HEAD
 
 const Menu = () => (
   <div>Menu Component</div>
 )
 
 export default Menu;
+=======
+import LunchMenu from './LunchMenu';
+import DinnerMenu from './DinnerMenu';
+import Sushi from './Sushi';
+import { Divider } from ''
+
+const Menu = () => (
+    <Div>
+
+        <h1>Lunch Menu</h1>
+        <LunchMenu />
+
+        <Divider />
+        <h1> Dinner Menu</h1>
+        < DinnerMenu />
+        <Divider />
+        <h1> Sushi Menu</h1>
+        <Sushi /> 
+
+    </Div>
+
+)
+
+export default Menu; 
+>>>>>>> menu sushi components
