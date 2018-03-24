b3e7bb788b2f5d9084cc316290ef14633c9c7ef7
Alex Shapiro <acjshapiro@gmail.com>
2018-03-24 14:53:45 -0600

    menus

--- client/src/components/DinnerMenu.js
+++ client/src/components/DinnerMenu.js
@@ -1,52 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Form from './Form';
+import { List } from 'semantic-ui-react';
 
 
 class DinnerMenu extends React.Component {
-    state = { menu_items: [], showForm: false }
+    state = { menu_items: [] }
 
-componentDidMount() {
-    axios.get('api/menus/1')
-    .then(res => this.setState({ menu_items: res.data})) 
-}
-
-    show() {
-       let { menu_items } = this.state
-       return (
-           <ul>
-               { menu_items.map( m =>
-                <li key={m.id}>
-                    <Link to={`/menu_items/${m.id}`}>{m.name}</Link>
-              </li>
-              )
-            }
-            </ul>)
-       }
-       form(){
-           return <Form submit={this.submit}/>
-       }
-
-       submit = (menu) => {
-           let { menu_items} = this.state
-           axios.post('/api/menu_items', { menu} )
-           .then( res => this.setState({ menu_items: [res.data, ...menu_items], showForm: false}))
-       }
-
-       toggleForm = () => {
-           this.setState( state => {
-               return { showForm: !state.showForm}
-           })
-       }
+    componentDidMount() {
+    axios.get('/api/menus/2')
+      .then( ({ data: menu_items }) => this.setState({ menu_items: menu_items }) )
+  }
 
        render() {
-           let { showForm } = this.state
+        const { menu_items } = this.state;
            return(
                <div>
-                   <h2>Menu Items</h2>
-                   <button onClick={this.toggleForm}>{ showForm ? 'Hide' : 'Show' } form</button>
-                   { showForm ? this.form() : this.show()}
+                   <List>
+                    { menu_items.map( menuItem => 
+                    <List.Item 
+                    key={menuItem.id}
+                    >
+                    {menuItem.name}
+                 </List.Item> 
+              )
+            }
+          </List>
+                   
                 </div>
            )
        }
@@ -57,4 +37,3 @@ componentDidMount() {
 
 
 export default DinnerMenu;
-
