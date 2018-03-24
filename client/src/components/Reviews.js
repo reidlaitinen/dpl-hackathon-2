b043fd1e00a8549d4ecbbb483d4dc2363d2cd554
Alicia Baker <aliciabaker314@gmail.com>
2018-03-24 14:47:33 -0600

    blah2

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -4,10 +4,6 @@ import banner from '../images/banner.png';
 
 const RatingExampleStar = () => (
   <Rating icon='star' defaultRating={3} maxRating={4} />
-
-
-
-  <div><Image src={banner} fluid/></div>
 )
 
 export default RatingExampleStar
