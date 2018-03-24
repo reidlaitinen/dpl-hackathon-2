a4e22550ae1f733d2e01cda815bf878db614dd44
Becca Tew <rebeccatew@gmail.com>
2018-03-24 16:30:46 -0600

    about updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import { Pagination } from 'semantic-ui-react';
-import { Segment, Image } from 'semantic-ui-react';
+import { Segment, Image, Container, Grid } from 'semantic-ui-react';
 import Chef from '../images/chef.jpg';
 import banner from '../images/banner.png';
 
 
-const ImageExampleCentered = () => (
-  <Segment>
-    <Image src={Chef} size='medium' centered />
-      <p align="center">
-      Originally from South Korea and adopted by an Oklahoman family, Danny Bowien is the chef and co-founder of Mission Chinese Food, based in New York and San Francisco. Starting his culinary career at various places on both coasts, Danny Bowien came to greater national attention when he worked at Farina and won the Pesto World Championship (who knew that was a thing?!).
+<Segment>
+  <Image src={Chef} size='medium' centered />
+    <p align="center">
+    Originally from South Korea and adopted by an Oklahoman family, Danny Bowien is the chef and co-founder of Mission Chinese Food, based in New York and San Francisco. Starting his culinary career at various places on both coasts, Danny Bowien came to greater national attention when he worked at Farina and won the Pesto World Championship (who knew that was a thing?!).
 
-      He describes his cooking as “Americanized Oriental food,” with dishes like supersmoky kung pao pastrami. Now, he focuses his time on Mission Chinese Food where only one dish costs more than $16, and 75 cents from each food item sold goes to a local food bank. Amazing chef and amazing person?I think yes.
-      </p>
-      <div><Image src={banner} fluid/></div>
-  </Segment>
+    He describes his cooking as “Americanized Oriental food,” with dishes like supersmoky kung pao pastrami. Now, he focuses his time on Mission Chinese Food where only one dish costs more than $16, and 75 cents from each food item sold goes to a local food bank. Amazing chef and amazing person?I think yes.
+    </p>
+    <div><Image src={banner} fluid/></div>
+</Segment>
+
+// <Grid.Row height={3}>
+//   <h1 style={{marginLeft: "50px"}}></h1>
+// </Grid.Row>
+
+
+
+const ImageExampleCentered = () => (
+  <Container>
+    <Grid>
+      <Grid.Row height={1}>
+        <p style={{marginLeft: "50px"}}></p>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <Segment>
+          <Image src={Chef} size='medium' centered />
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={8}>
+        <h1 align="center">Our Amazing Chef!</h1>
+        <p align="center">
+        Originally from South Korea and adopted by an Oklahoman family, Danny Bowien is the chef and co-founder of Mission Chinese Food, based in New York and San Francisco. Starting his culinary career at various places on both coasts, Danny Bowien came to greater national attention when he worked at Farina and won the Pesto World Championship (who knew that was a thing?!).
+
+        He describes his cooking as “Americanized Oriental food,” with dishes like supersmoky kung pao pastrami. Now, he focuses his time on Mission Chinese Food where only one dish costs more than $16, and 75 cents from each food item sold goes to a local food bank. Amazing chef and amazing person?I think yes.
+        </p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+    <span><Image src={banner} fluid/></span>
+  </Container>
 )
 
 
--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -3,22 +3,6 @@ import { Header } from 'semantic-ui-react';
 import { Pagination } from 'semantic-ui-react';
 import { Segment, Image, Container, Grid } from 'semantic-ui-react';
 
-// locationleft = {
-//   display: flex;
-//   justify-content: left;
-// }
-
-// TESTING 123
-//   <div>
-//     SIDE BAR
-//   </div>
-//   </Container>
-//
-//   <Container column=2>
-//   <div>
-//     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3026.40235220016!2d-111.8432572845965!3d40.665102879336985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8752621d97d5f405%3A0x800c6a89b50c2ea3!2s4810+Highland+Cir%2C+Salt+Lake+City%2C+UT+84117!5e0!3m2!1sen!2sus!4v1521921914824" width="600" height="450" frameborder="0" allowfullscreen></iframe>
-//   </div>
-//   </Grid>
 
 const Location = () => (
     <Container>
