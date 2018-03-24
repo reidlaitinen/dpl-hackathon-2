e47c8e11e92f640359c11c6ebd13f296ea79491f
Alex Shapiro <acjshapiro@gmail.com>
2018-03-24 14:21:37 -0600

    form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324170628) do
+ActiveRecord::Schema.define(version: 20180324201432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,14 +18,10 @@ ActiveRecord::Schema.define(version: 20180324170628) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "category"
     t.text "description"
-    t.bigint "menu_id"
-    t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_items_on_cart_id"
-    t.index ["menu_id"], name: "index_items_on_menu_id"
+    t.string "category"
   end
 
   create_table "menus", force: :cascade do |t|
@@ -65,5 +61,4 @@ ActiveRecord::Schema.define(version: 20180324170628) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  add_foreign_key "items", "menus"
 end
